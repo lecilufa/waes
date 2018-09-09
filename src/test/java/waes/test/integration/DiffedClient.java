debcee12fc1febbb2d941fb5dceecbf629d6c2f4
Nicolas <66002172@qq.com>
2018-09-09 16:14:12 +0800

    hhh

--- src/test/java/waes/test/integration/DiffedClient.java
+++ src/test/java/waes/test/integration/DiffedClient.java
@@ -173,4 +173,184 @@ public class DiffedClient {
 		assertEquals(new Integer(2),length);
 	}
 	
+	@Test
+	public void equal() throws Exception {
+		Long id = 13L;
+		
+		Text text = new Text();
+		text.setId(id);
+		String leftText = "123456";
+		text.setLeftText(leftText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
+		Response response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/left");
+		
+		text = new Text();
+		text.setId(id);
+		String rightText = "123456";
+		text.setRightText(rightText);
+		
+		encodeString = Base64JsonUtil.encodeToString(text);		
+		response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/right");
+		
+		response = given()
+				.get("http://localhost:8080/v1/diff/"+ id);
+		
+		JsonPath json = response.jsonPath();
+		
+		Long resultId = json.getLong("id");
+		String status = json.getString("status");
+		List<Map<String,Object>> diffs = json.getList("diffs");
+		Integer statusCode = response.getStatusCode();
+		
+		assertEquals(new Integer( HttpStatus.OK.value()), statusCode);
+		assertEquals(id, resultId);
+		assertEquals("EQUAL",status);
+		assertTrue(diffs.size() == 0);
+				
+	}
+	
+	@Test
+	public void notSameSize() throws Exception {
+		Long id = 14L;
+		
+		Text text = new Text();
+		text.setId(id);
+		String leftText = "0123456789abcdefg";
+		text.setLeftText(leftText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
+		Response response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/left");
+		
+		text = new Text();
+		text.setId(id);
+		String rightText = "0123456789abcdefg***";
+		text.setRightText(rightText);
+		
+		encodeString = Base64JsonUtil.encodeToString(text);		
+		response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/right");
+		
+		response = given()
+				.get("http://localhost:8080/v1/diff/"+ id);
+		
+		JsonPath json = response.jsonPath();
+		
+		Long resultId = json.getLong("id");
+		String status = json.getString("status");
+		List<Map<String,Object>> diffs = json.getList("diffs");
+		Integer statusCode = response.getStatusCode();
+		
+		assertEquals(new Integer( HttpStatus.OK.value()), statusCode);
+		assertEquals(id, resultId);
+		assertEquals("NOT_SAME_SIZE",status);
+		assertTrue(diffs.size() == 0);
+				
+	}
+	
+	@Test
+	public void emptyText() throws Exception {
+		
+		Long id = 100L;
+		
+		Response response = given()
+				.get("http://localhost:8080/v1/diff/"+ id);
+		
+		response.print();
+		
+		JsonPath json = response.jsonPath();
+		
+		Integer statusCode = response.getStatusCode();
+		String excpetion = json.getString("exception");
+		String message = json.getString("message");
+		Integer code = json.getInt("code");
+		
+		assertEquals(new Integer( HttpStatus.PRECONDITION_REQUIRED.value()), statusCode);
+		assertEquals("waes.task.exception.PreconditionException", excpetion);
+		assertEquals("No text with given id",message);
+		assertEquals(new Integer( HttpStatus.PRECONDITION_REQUIRED.value()),code);
+		
+	}
+	
+	@Test
+	public void leftTextEmpty() throws Exception {
+		Long id = 15L;
+	
+		Text text = new Text();
+		text.setId(id);
+		String rightText = "0123**6789abcdefg";
+		text.setRightText(rightText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
+		Response response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/right");
+		
+		response = given()
+				.get("http://localhost:8080/v1/diff/"+ id);
+		
+		response.print();
+		
+		JsonPath json = response.jsonPath();
+		
+		Integer statusCode = response.getStatusCode();
+		String excpetion = json.getString("exception");
+		String message = json.getString("message");
+		Integer code = json.getInt("code");
+		
+		assertEquals(new Integer( HttpStatus.PRECONDITION_REQUIRED.value()), statusCode);
+		assertEquals("waes.task.exception.PreconditionException", excpetion);
+		assertEquals("Left text should not be null",message);
+		assertEquals(new Integer( HttpStatus.PRECONDITION_REQUIRED.value()),code);
+		
+	}
+	
+	
+	@Test
+	public void rightTextEmpty() throws Exception {
+		
+		Long id = 16L;
+		
+		Text text = new Text();
+		text.setId(id);
+		String leftText = "0123**6789abcdefg";
+		text.setLeftText(leftText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
+		Response response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/left");
+		
+		response = given()
+				.get("http://localhost:8080/v1/diff/"+ id);
+		
+		response.print();
+		
+		JsonPath json = response.jsonPath();
+		
+		Integer statusCode = response.getStatusCode();
+		String excpetion = json.getString("exception");
+		String message = json.getString("message");
+		Integer code = json.getInt("code");
+		
+		assertEquals(new Integer( HttpStatus.PRECONDITION_REQUIRED.value()), statusCode);
+		assertEquals("waes.task.exception.PreconditionException", excpetion);
+		assertEquals("Right text should not be null",message);
+		assertEquals(new Integer( HttpStatus.PRECONDITION_REQUIRED.value()),code);
+	}
+	
+	
 }
