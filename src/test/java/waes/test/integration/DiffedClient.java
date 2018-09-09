ecac71e315e2a331745292d990c95cb57c99020c
Nicolas <66002172@qq.com>
2018-09-09 15:25:32 +0800

    77

--- src/test/java/waes/test/integration/DiffedClient.java
+++ src/test/java/waes/test/integration/DiffedClient.java
@@ -1,25 +1,176 @@
 package waes.test.integration;
 
-import static io.restassured.RestAssured.*;
-import static org.junit.Assert.*;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import waes.task.model.Text;
+import waes.task.util.Base64JsonUtil;
 
-@Ignore
+
 public class DiffedClient {
-
-	@Test 
-	public void saveLeft(){
+	
+	@Test
+	public void saveLeft() throws Exception {
+		
+		Long id = 11L;
+		
+		// text create
+		Text text = new Text();
+		text.setId(id);
+		String leftText = "0123456789123456";
+		text.setLeftText(leftText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
 		
 		Response response = given()
-                .get("http://localhost:8080/v1/diff/1");
-       // response.print();
-        
-        String status = response.jsonPath().getString("status");
-        assertEquals("DIFF", status);
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/left");
+		
+		response.print();
+		
+		JsonPath json = response.jsonPath();
+		Long actualId = json.getLong("id");
+		Integer statusCode = response.getStatusCode();
+				
+		assertEquals(leftText, json.getString("leftText"));
+		assertEquals(id, actualId);
+		assertEquals(new Integer( HttpStatus.CREATED.value()), statusCode);
+		
+		// text update
+		text = new Text();
+		text.setId(id);
+		leftText = "012345678912**56";
+		text.setLeftText(leftText);
+		
+		encodeString = Base64JsonUtil.encodeToString(text);		
+		
+		response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/left");
+		
+
+		json = response.jsonPath();
+		actualId = json.getLong("id");
+		statusCode = response.getStatusCode();
+				
+		assertEquals(leftText, json.getString("leftText"));
+		assertEquals(id, actualId);
+		assertEquals(new Integer( HttpStatus.CREATED.value()), statusCode);
+	}
+	
+
+	@Test
+	public void saveRight() throws Exception {
+		
+		Long id = 12L;
+		
+		// text create
+		Text text = new Text();
+		text.setId(id);
+		String rightText = "0123**6789123456";
+		text.setRightText(rightText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
+		
+		Response response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/right");
+		
+		response.print();
+		
+		JsonPath json = response.jsonPath();
+		Long actualId = json.getLong("id");
+		Integer statusCode = response.getStatusCode();
+				
+		assertEquals(rightText, json.getString("rightText"));
+		assertEquals(id, actualId);
+		assertEquals(new Integer( HttpStatus.CREATED.value()), statusCode);
+		
+		// text update
+		text = new Text();
+		text.setId(id);
+		rightText = "0123ww6789123456";
+		text.setRightText(rightText);
+		
+		encodeString = Base64JsonUtil.encodeToString(text);		
+		
+		response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/right");
+		
+
+		json = response.jsonPath();
+		actualId = json.getLong("id");
+		statusCode = response.getStatusCode();
+				
+		assertEquals(rightText, json.getString("rightText"));
+		assertEquals(id, actualId);
+		assertEquals(new Integer( HttpStatus.CREATED.value()), statusCode);
+	}
+	
+	@Test
+	public void diffed() throws Exception {
+		
+		Long id = 13L;
+		
+		Text text = new Text();
+		text.setId(id);
+		String leftText = "0123456789abcdefg";
+		text.setLeftText(leftText);
+		
+		String encodeString = Base64JsonUtil.encodeToString(text);		
+		Response response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/left");
+		
+		
+		text = new Text();
+		text.setId(id);
+		String rightText = "0123**6789abcdefg";
+		text.setRightText(rightText);
+		
+		encodeString = Base64JsonUtil.encodeToString(text);		
+		response = given()
+				.contentType(ContentType.TEXT)
+				.body(encodeString)
+				.put("http://localhost:8080/v1/diff/"+ id +"/right");
+		
+		
+		response = given()
+				.get("http://localhost:8080/v1/diff/"+ id);
+		
+		JsonPath json = response.jsonPath();
+		
+		Long resultId = json.getLong("id");
+		String status = json.getString("status");
+		List<Map<String,Object>> diffs = json.getList("diffs");
+		Integer statusCode = response.getStatusCode();
+		
+		assertEquals(new Integer( HttpStatus.OK.value()), statusCode);
+		assertEquals(id, resultId);
+		assertEquals("DIFF",status);
+		assertTrue(diffs.size() > 0);
+				
+		Integer offset = (Integer) diffs.get(0).get("offset");
+		Integer length = (Integer) diffs.get(0).get("length");
+		
+		assertEquals(new Integer(4),offset);
+		assertEquals(new Integer(2),length);
 	}
 	
 }
